16dc5a490f7745df66de3d28c634cc84955565fe
Pinouf42 <julien.piney@gmail.com>
2013-03-23 20:59:57 +0100

    Changement menu

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -2,10 +2,9 @@
 
 $menu = array(
     0 => "Liste des notes non terminées|liste_note.php",
-    1 => "Créer une note|saisie_note.php",
-    2 => "Historique des notes|historique.php",
-    3 => "Validation|validation.php",
-    4 => "À propos de nous|about.html",
+    1 => "Historique des notes|historique.php",
+    2 => "Validation|validation.php",
+    3 => "À propos de nous|about.html",
     );
 
 ?>
