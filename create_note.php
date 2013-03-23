9ec418f7dc708b73a6a8a368e3d5217d19d4ca4a
Pinouf42 <julien.piney@gmail.com>
2013-03-23 21:57:56 +0100

    Update create_note.php

--- create_note.php
+++ create_note.php
@@ -2,7 +2,7 @@
 include_once('include/config.php');
 mssql_connect($host, $username, $password);
 mssql_select_db($database);
-mssql_query("INSERT INTO NOTE_FRAIS(datesoumission_note, commentaire_note, id_pers, clos_note) VALUES (getDate(), 'Note du '+CONVERT(CHAR(10),  GETDATE(), 120), '1', '0')");
+mssql_query("INSERT INTO NOTE_FRAIS(datesoumission_note, commentaire_note, id_pers, clos_note) VALUES (getDate(), 'Note du '+CONVERT(CHAR(10),  GETDATE(), 120), '3', '0')");
 $query_id_note = "SELECT MAX(id_note) FROM NOTE_FRAIS";
 $result_query = mssql_query($query_id_note);
 $res = mssql_fetch_array($result_query);
