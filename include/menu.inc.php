639a2c1167637755a845066cfba4144f40e8b815
Pinouf42 <julien.piney@gmail.com>
2013-03-23 21:00:32 +0100

    Update menu.inc.php

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 $menu = array(
-    0 => "Liste des notes non terminées|liste_note.php",
+    0 => "Liste de vos notes|liste_note.php",
     1 => "Historique des notes|historique.php",
     2 => "Validation|validation.php",
     3 => "À propos de nous|about.html",
