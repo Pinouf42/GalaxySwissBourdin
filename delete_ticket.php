396287a0f37c3f7790cb1a250078fa0a41ea7d98
formation <formation@Ubuntu13>
2013-04-09 13:29:36 +0200

    include

--- delete_ticket.php
+++ delete_ticket.php
@@ -1,5 +1,5 @@
 <?php
-include_once('include/config.php');
+include_once('config.php');
 mssql_connect($host, $username, $password);
 mssql_select_db($database);
 mssql_query("DELETE FROM JUSTIFICATIF WHERE id_justif = '".stripslashes($_POST['justif'])."'");
--- include/cloturer_note.php
+++ include/cloturer_note.php
@@ -1,5 +1,5 @@
 <?php
-include_once('include/config.php');
+include_once('config.php');
 mssql_connect($host, $username, $password);
 mssql_select_db($database);
 mssql_query("UPDATE NOTE_FRAIS SET clos_note = 1 WHERE id_note = ".stripslashes($_GET['note'])."");
