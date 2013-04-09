31fea446805a8b77019ec49725e28c41bffb7a31
formation <formation@Ubuntu13>
2013-04-09 13:28:39 +0200

    Upload

--- include/cloturer_note.php
+++ include/cloturer_note.php
@@ -4,5 +4,5 @@ mssql_connect($host, $username, $password);
 mssql_select_db($database);
 mssql_query("UPDATE NOTE_FRAIS SET clos_note = 1 WHERE id_note = ".stripslashes($_GET['note'])."");
 mssql_close();
-header('Location: liste_note.php');
+header('Location: ../liste_note.php');
 ?>
