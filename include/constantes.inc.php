2ef9ac6d4eedb59176737ab6a8665d0073378622
formation <formation@ubuntu2>
2013-04-12 13:07:59 +0200

    modification

--- include/constantes.inc.php
+++ include/constantes.inc.php
@@ -2,9 +2,9 @@
 
 define('NOM','GalaxySwissBourdin');
 
-define('RACINE', '/var/www/'.NOM);
+define('RACINE', '/var/www/'.NOM.'/');
 define('HTML', RACINE.'/html/');
 
-define('RACINE_SITE', 'http://'.$_SERVER['SERVER_NAME'].'/'.NOM);
+define('RACINE_SITE', 'http://'.$_SERVER['SERVER_NAME'].'/'.NOM.'/');
 define('SITE_PRIVATE', RACINE_SITE.'privates/')
 ?>
