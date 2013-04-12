d12c0e9ec167e0c7cec23c431841044ced8ba4bd
formation <formation@ubuntu2>
2013-04-12 11:55:27 +0200

    modification pour l'adresse web dynamique

--- include/constantes.inc.php
+++ include/constantes.inc.php
@@ -3,6 +3,6 @@
 define('RACINE', '/var/www/GalaxySwissBourdin/');
 define('HTML', RACINE.'/html/');
 
-define('RACINE_SITE', 'http://localhost/GalaxySwissBourdin/');
+define('RACINE_SITE', 'http://'.$_SERVER['SERVER_NAME'].'/GalaxySwissBourdin/');
 define('SITE_PRIVATE', RACINE_SITE.'privates/')
 ?>
