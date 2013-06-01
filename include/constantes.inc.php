ba276974b5c0c9d1513a5b064e4fe3082db4a502
formation <formation@formation-VirtualBox>
2013-06-01 12:13:16 +0200

    Configuration pour fonctionnement CFAI et LOCAL

--- include/constantes.inc.php
+++ include/constantes.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+// CFAI
+
+/*
 define('NOM','~equipe3');
 
 define('RACINE', '/home/equipe3/site_web/');
@@ -7,8 +10,10 @@ define('HTML', RACINE.'/html/');
 
 define('RACINE_SITE', 'http://'.$_SERVER['SERVER_NAME'].'/'.NOM.'/');
 define('SITE_PRIVATE', RACINE_SITE.'privates/')
+*/
 
-/*
+// LOCAL
+
 define('NOM','GalaxySwissBourdin');
 
 define('RACINE', '/var/www/'.NOM.'/');
@@ -16,6 +21,6 @@ define('HTML', RACINE.'/html/');
 
 define('RACINE_SITE', 'http://'.$_SERVER['SERVER_NAME'].'/'.NOM.'/');
 define('SITE_PRIVATE', RACINE_SITE.'privates/')
-*/
+
 
 ?>
